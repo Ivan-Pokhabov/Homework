a0e94665752b096cbf3008854a13740b812b92b4
Ivan-Pokhabov <vanek3372@gmail.com>
2023-10-10 01:59:00 +0300

    Создай свой заголовочный файл с функциями чтения массива из файла и быстрой сортировки. Реализуй модуль с этими функциями. Переделай задачу из прошлого дз под ввод из файла

---
+++ ModulesAndFilesTask/fileQsort.h
@@ -0,0 +1,7 @@
+#pragma once
+
+//Функция для чтения массива из файла
+void arrayReader(const char *fileName, int *arraySize, int array[]);
+
+//Функция сортировки
+void qqsort(int array[], const int leftBorder, const int rightBorder);
