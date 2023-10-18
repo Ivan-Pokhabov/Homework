f1d09cf6e887dff00de3fc0231e70ef7214a4cd8
Ivan-Pokhabov <vanek3372@gmail.com>
2023-10-18 23:03:44 +0300

    Создай файл с черновым решением задачи продвинутая скобочна последовательность

---
+++ AdvancedBracketBalance/AdvancedBracketBalance.c
@@ -0,0 +1,59 @@
+﻿#include "..\Stack\CharStack.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool bracketCompare(char leftBracket, char rightBracket)
+{
+    switch (leftBracket)
+    {
+        case '{':
+            return rightBracket == '}';
+        case '(':
+            return rightBracket == ')';
+        case '[':
+            return rightBracket == ']';
+    }
+}
+
+int main()
+{
+    CharStack* bracketStack = NULL;
+    char symbol = getchar();
+    while (symbol != '\n' || symbol != EOF)
+    {
+        while (symbol == ' ')
+        {
+            symbol = getchar();
+        }
+        if (symbol == '\n' || symbol == EOF)
+        {
+            break;
+        }
+        if (symbol == '(' || symbol == '{' || symbol == '[')
+        {
+            pushChar(&bracketStack, symbol);
+        }
+        else if (symbol == ')' || symbol == '}' || symbol == ']')
+        {
+            if (bracketStack == NULL || !bracketCompare(topChar(&bracketStack), symbol))
+            {
+                printf("Not balanced");
+                return 0;
+            }
+            popChar(&bracketStack);
+        }
+        else
+        {
+            printf("Input is invalid");
+        }
+        symbol = getchar();
+    }
+    if (bracketStack != NULL)
+    {
+        printf("Not balanced");
+        clearChar(&bracketStack);
+        return 0;
+    }
+    printf("Balanced");
+}
