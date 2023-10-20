9af7694c2496bb5e8e2dfaf3c0f413b1cd00c1cf
Ivan-Pokhabov <vanek3372@gmail.com>
2023-10-20 22:01:55 +0300

    Make individual fuction of checking balaced. Make file entering for testing. Write function for testing solve and tests in txt-files. Add error processing of solve.

--- AdvancedBracketBalance/AdvancedBracketBalance.c
+++ AdvancedBracketBalance/AdvancedBracketBalance.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+typedef enum ErrorCode
+{
+    ok,
+    invalidInput
+} ErrorCode;
+
 bool bracketCompare(char leftBracket, char rightBracket)
 {
     switch (leftBracket)
@@ -16,21 +22,21 @@ bool bracketCompare(char leftBracket, char rightBracket)
     }
 }
 
-int main()
+bool isBalanced(FILE* file, ErrorCode *errorCode)
 {
     CharStack* bracketStack = NULL;
-    char symbol = getchar();
+    char symbol = getc(file);
     while (symbol != '\n' || symbol != EOF)
     {
         while (symbol == ' ')
         {
-            symbol = getchar();
+            symbol = getc(file);
         }
         if (symbol == '\n' || symbol == EOF)
         {
             break;
         }
-        if (symbol == '(' || symbol == '{' || symbol == '[')
+        else if (symbol == '(' || symbol == '{' || symbol == '[')
         {
             pushChar(&bracketStack, symbol);
         }
@@ -38,22 +44,86 @@ int main()
         {
             if (bracketStack == NULL || !bracketCompare(topChar(&bracketStack), symbol))
             {
-                printf("Not balanced");
-                return 0;
+                return false;
             }
             popChar(&bracketStack);
         }
         else
         {
-            printf("Input is invalid");
+            *errorCode = invalidInput;
+            return false;
         }
-        symbol = getchar();
+        symbol = getc(file);
     }
     if (bracketStack != NULL)
     {
-        printf("Not balanced");
+        return false;
         clearChar(&bracketStack);
+    }
+    return true;
+}
+
+bool test(void)
+{
+    FILE* file = NULL;
+    fopen_s(&file, "test1.txt", "r");
+    ErrorCode errorCode = ok;
+    bool errors[4] = { true, true, true, true };
+    if (!isBalanced(file, &errorCode) || errorCode != ok)
+    {
+        printf("Program is not working with test case 1");
+        errors[0] = false;
+    }
+    fopen_s(&file, "test2.txt", "r");
+    errorCode = ok;
+    if (!isBalanced(file, &errorCode) || errorCode != ok)
+    {
+        printf("Program is not working with test case 2");
+        errors[1] = false;
+    }
+    fopen_s(&file, "test3.txt", "r");
+    errorCode = ok;
+    if (isBalanced(file, &errorCode) || errorCode != ok)
+    {
+        printf("Program is not working with test case 3");
+        errors[2] = false;
+    }
+    fopen_s(&file, "test4.txt", "r");
+    errorCode = ok;
+    if (!isBalanced(file, &errorCode) && errorCode == ok)
+    {
+        printf("Program is not working with test case 4");
+        errors[3] = false;
+    }
+    bool compliteTests = true;
+    for (int i = 0; i < 4; ++i)
+    {
+        if (!errors[i])
+        {
+            printf("Program is not working with test case %d\n", i + 1);
+            compliteTests = false;
+        }
+    }
+    return compliteTests;
+}
+
+int main()
+{
+    ErrorCode errorCode = ok;
+    if (!test())
+    {
         return 0;
     }
-    printf("Balanced");
+    if (isBalanced(stdin, &errorCode))
+    {
+        printf("Your bracket subsequence is balanced");
+    }
+    else if (errorCode == invalidInput)
+    {
+        printf("Your input is invalid");
+    }
+    else
+    {
+        printf("Your bracket subsequence is not balanced")
+    }
 }
