e19c1682b6b63807ad7b8088b0647725947a86ab
Ivan-Pokhabov <vanek3372@gmail.com>
2023-12-03 21:01:07 +0300

    Make module with binary heap

---
+++ BinaryHeap/BinaryHeap.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdlib.h>
+
+typedef struct BinaryHeap BinaryHeap;
+
+typedef enum ErrorCode
+{
+    ok,
+    nullptr,
+    memoryError,
+    emptyHeap
+} ErrorCode;
+
+BinaryHeap* createHeap(void);
+
+int add(BinaryHeap* const heap, const int key, const int value);
+
+int pop(BinaryHeap* const heap, int* const key, int* const value);
+
+int build(BinaryHeap* const heap, const int const keys[], const int const values[], const size_t size);
+
+void deleteHeap(BinaryHeap** heap);
