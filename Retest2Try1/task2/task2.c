6b61f6544eb379a0245ad88ec2a7cfcbfea0eecb
Ivan-Pokhabov <vanek3372@gmail.com>
2023-12-08 15:24:31 +0300

    Add changes to make task complite

--- Retest2Try1/task2/task2.c
+++ Retest2Try1/task2/task2.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define MEMEORY_ERROR -1
+#define VERTEX_NUMBER 3
+#define EDGES_NUMBER 3
+
 void dfs(const int vertex, const int* const* const graph, bool** visited, const size_t vertexNumber)
 {
     if ((*visited)[vertex])
@@ -20,32 +24,59 @@ void dfs(const int vertex, const int* const* const graph, bool** visited, const 
     }
 }
 
-int makeAdjacencyList(int*** graph, const size_t edgesNumber, const size_t vertexNumber, const int matrix[3][3])
+int makeAdjacencyList(int*** adjacencyList, const size_t edgesNumber, const size_t vertexNumber, const int matrix[VERTEX_NUMBER][EDGES_NUMBER])
 {
+    int** graph = (int**)calloc(VERTEX_NUMBER, sizeof(int*));
+    if (graph == NULL)
+    {
+        return -1;
+    }
+    for (size_t i = 0; i < VERTEX_NUMBER; ++i)
+    {
+        graph[i] = (int*)calloc(VERTEX_NUMBER, sizeof(int));
+        if (graph[i] == NULL)
+        {
+            for (size_t j = 0; j < i; ++j)
+            {
+                free(graph[j]);
+            }
+            free(graph);
+            return -1;
+        }
+        for (size_t j = 0; j < VERTEX_NUMBER; ++j)
+        {
+            graph[i][j] = -1;
+        }
+    }
     int** edges = (int**)calloc(edgesNumber, sizeof(int*));
     if (edges == NULL)
     {
-        return -1;
+        return MEMEORY_ERROR;
     }
     for (size_t i = 0; i < edgesNumber; ++i)
     {
         edges[i] = (int*)calloc(vertexNumber, sizeof(int));
         if (edges[i] == NULL)
         {
-            return -1;
+            for (size_t j = 0; j < i; ++j)
+            {
+                free(edges[j]);
+            }
+            free(edges);
+            return MEMEORY_ERROR;
         }
         for (size_t j = 0; j < vertexNumber; ++j)
         {
             edges[i][j] = -1;
         }
     }
-    for (size_t i = 0; i < 3; ++i)
+    for (size_t i = 0; i < vertexNumber; ++i)
     {
-        for (size_t j = 0; j < 3; ++j)
+        for (size_t j = 0; j < edgesNumber; ++j)
         {
             if (matrix[i][j] == 1)
             {
-                for (size_t k = 0; k < 3; ++k)
+                for (size_t k = 0; k < vertexNumber; ++k)
                 {
                     if (edges[j][k] == -1)
                     {
@@ -54,19 +85,19 @@ int makeAdjacencyList(int*** graph, const size_t edgesNumber, const size_t verte
                     }
                     else
                     {
-                        for (size_t h = 0; h < 3; ++h)
+                        for (size_t h = 0; h < vertexNumber; ++h)
                         {
-                            if ((*graph)[i][h] == -1)
+                            if (graph[i][h] == -1)
                             {
-                                (*graph)[i][h] = edges[j][k];
+                                graph[i][h] = edges[j][k];
                                 break;
                             }
                         }
-                        for (size_t h = 0; h < 3; ++h)
+                        for (size_t h = 0; h < vertexNumber; ++h)
                         {
-                            if ((*graph)[edges[j][k]][h] == -1)
+                            if (graph[edges[j][k]][h] == -1)
                             {
-                                (*graph)[edges[j][k]][h] = i;
+                                graph[edges[j][k]][h] = i;
                                 break;
                             }
                         }
@@ -80,47 +111,41 @@ int makeAdjacencyList(int*** graph, const size_t edgesNumber, const size_t verte
         free(edges[i]);
     }
     free(edges);
+    *adjacencyList = graph;
+    return 0;
 }
 
 int main()
 {
-    const int matrix[3][3] = { {1, 0, 0}, {1, 0, 0}, {0, 0, 0} };
-    int** graph = (int**)calloc(3, sizeof(int*));
-    if (graph == NULL)
+    const int matrix[VERTEX_NUMBER][EDGES_NUMBER] = { {1, 0, 0}, {1, 0, 0}, {0, 0, 0} };
+    int** graph = 0;
+    if (makeAdjacencyList(&graph, EDGES_NUMBER, VERTEX_NUMBER, matrix) != 0)
     {
-        return -1;
-    }
-    for (size_t i = 0; i < 3; ++i)
-    {
-        graph[i] = (int*)calloc(3, sizeof(int));
-        if (graph[i] == NULL)
+        for (size_t i = 0; i < VERTEX_NUMBER; ++i)
         {
-            return -1;
+            free(graph[i]);
         }
-        for (size_t j = 0; j < 3; ++j)
-        {
-            graph[i][j] = -1;
-        }
+        free(graph);
+        return MEMEORY_ERROR;
     }
-    makeAdjacencyList(&graph, 3, 3, matrix);
-    bool* visited = (bool*)calloc(3, sizeof(bool));
+    bool* visited = (bool*)calloc(VERTEX_NUMBER, sizeof(bool));
     size_t countComponents = 1;
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < VERTEX_NUMBER; ++i)
     {
         if (visited[i])
         {
             continue;
         }
-        printf("Component %d, vertexes - ", countComponents + 1);
-        dfs(i, graph, &visited, 3);
+        printf("Component %d, vertexes - ", countComponents);
+        dfs(i, graph, &visited, VERTEX_NUMBER);
         printf("\n");
         ++countComponents;
     }
-    for (size_t i = 0; i < 3; ++i)
+    for (size_t i = 0; i < VERTEX_NUMBER; ++i)
     {
         free(graph[i]);
     }
     free(graph);
-    free(visited)
+    free(visited);
 }
 
