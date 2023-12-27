533eaf1bea3f289a891fe239dcc7c152fe913817
Ivan-Pokhabov <vanek3372@gmail.com>
2023-12-27 12:57:04 +0300

    Minor changes

--- RealNumbersLexer/Lexer.c
+++ RealNumbersLexer/Lexer.c
@@ -17,7 +17,7 @@ typedef enum Stage
 
 bool lexer(const char* const string)
 {
-    Stage state = 0;
+    Stage state = nothing;
     for (size_t i = 0; i == 0 || string[i - 1] != '\0'; ++i)
     {
         switch (state)
@@ -25,69 +25,69 @@ bool lexer(const char* const string)
         case nothing:
             if (isdigit(string[i]))
             {
-                state = 1;
+                state = firstDigit;
                 break;
             }
             return false;
         case firstDigit:
             if (isdigit(string[i]))
             {
-                state = 1;
+                state = firstDigit;
                 break;
             }
             if (string[i] == '.')
             {
-                state = 2;
+                state = dot;
                 break;
             }
             if (string[i] == 'E')
             {
-                state = 4;
+                state = stateOnE;
                 break;
             }
             return string[i] == '\0';
         case dot:
             if (isdigit(string[i]))
             {
-                state = 3;
+                state = secondDigit;
                 break;
             }
             return false;
         case secondDigit:
             if (isdigit(string[i]))
             {
-                state = 3;
+                state = secondDigit;
                 break;
             }
             if (string[i] == 'E')
             {
-                state = 4;
+                state = stateOnE;
                 break;
             }
             return string[i] == '\0';
         case stateOnE:
             if (isdigit(string[i]))
             {
-                state = 6;
+                state = thirdDigit;
                 break;
             }
             if (string[i] == '+' || string[i] == '-')
             {
-                state = 5;
+                state = plusOrMinus;
                 break;
             }
             return false;
         case plusOrMinus:
             if (isdigit(string[i]))
             {
-                state = 6;
+                state = thirdDigit;
                 break;
             }
             return false;
         case thirdDigit:
             if (isdigit(string[i]))
             {
-                state = 6;
+                state = thirdDigit;
                 break;
             }
             return string[i] == '\0';
